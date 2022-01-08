36ba9799335d7137f7ffe0a5fe7aa6a551087702
JanHBade <github@1wiki.de>
2022-01-08 20:49:55 +0100

    Update moveAudio.sh

--- moveAudio.sh
+++ moveAudio.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "AUDIO RipRobot: Dateien werden auf USB-Stick verschoben"
-sudo rsync --progress --modify-window=1 --update --recursive --times --ignore-existing /home/pi/RipRobot/tmp/ /media/usb/ >/home/pi/RipRobot/rsync.log
+sudo rsync --progress --modify-window=1 --update --recursive --times --ignore-existing /home/pi/RipRobot/tmp/ /home/pi/Samba/Rasperry Pi >/home/pi/RipRobot/rsync.log
 echo "AUDIO RipRobot: Dateien wurden auf USB-Stick verschoben"
