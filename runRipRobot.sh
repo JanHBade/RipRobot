cb17a6d7e3db872d517c7b7210483d88b90e0241
JanHBade <github@1wiki.de>
2022-01-08 20:55:51 +0100

    Update runRipRobot.sh

--- runRipRobot.sh
+++ runRipRobot.sh
@@ -3,4 +3,4 @@ echo "AUDIO RipRobot: Alle Temporäre Dateien werden vor dem Start gelöscht"
 uptime
 sudo rm -r /home/pi/RipRobot/tmp/*
 cd /home/pi/RipRobot
-sudo python ripRobot.py /home/pi/RipRobot/tmp 180
+sudo python3 ripRobot.py /home/pi/RipRobot/tmp 180
